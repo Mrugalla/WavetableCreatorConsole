8f06ce6f4873f449e922427ddeea383cfa460a17
Mrugalla <trader-joes3@web.de>
2023-12-23 17:37:57 +0100

    idk lol

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,6 +1,8 @@
 #include <JuceHeader.h>
 #include <functional>
 
+#define oopsie(x) jassert(!(x))
+
 namespace wtc
 {
     static constexpr float pi = 3.14159265359f;
@@ -46,6 +48,188 @@ namespace wtc
             a -= b;
         return a;
     }
+
+    struct BiquadFilter
+    {
+        enum Type { LP, HP, BP, AP, NOTCH, PEAK, LS, HS };
+        static constexpr double DefaultCutoffFc = 1000. / 44100.;
+        static constexpr double DefaultResonance = .1;
+
+        BiquadFilter() :
+            type(Type::BP),
+            bCoef{ 0., 0., 0. },
+            aCoef{ 0., 0. },
+            w{ 0., 0. },
+            omega(DefaultCutoffFc),
+            cosOmega(0.), sinOmega(0.),
+            Q(DefaultResonance), alpha(0.), A(1.),
+            a{ 0., 0., 0. },
+            b{ 0., 0., 0. }
+        {
+        }
+
+        void prepare() noexcept
+        {
+            update();
+        }
+
+        void operator()(double* samples, int numSamples) noexcept
+        {
+            for (auto s = 0; s < numSamples; ++s)
+                processSample(samples[s]);
+        }
+
+        double operator()(double sample) noexcept
+        {
+            return processSample(sample);
+        }
+
+        /* fc = hz / sampleRate */
+        void setCutoffFc(double fc) noexcept
+        {
+            omega = static_cast<double>(tau) * fc;
+        }
+
+        void setResonance(double q) noexcept
+        {
+            Q = q;
+        }
+
+        void setType(Type _type) noexcept
+        {
+            type = _type;
+        }
+
+        void update() noexcept
+        {
+            cosOmega = cos(omega);
+            sinOmega = sin(omega);
+
+            switch (type)
+            {
+            case LP:
+                alpha = sinOmega / (2. * Q);
+                b[0] = (1. - cosOmega) / 2.;
+                b[1] = 1. - cosOmega;
+                b[2] = b[0];
+                a[0] = 1. + alpha;
+                a[1] = -2. * cosOmega;
+                a[2] = 1. - alpha;
+                break;
+            case HP:
+                alpha = sinOmega / (2. * Q);
+                b[0] = (1. + cosOmega) / 2.;
+                b[1] = -(1. + cosOmega);
+                b[2] = b[0];
+                a[0] = 1. + alpha;
+                a[1] = -2. * cosOmega;
+                a[2] = 1. - alpha;
+                break;
+            case BP:
+                alpha = sinOmega * sinh(log(2.) / 2. * Q * omega / sinOmega);
+                b[0] = sinOmega / 2.;
+                b[1] = 0.;
+                b[2] = -b[0];
+                a[0] = 1. + alpha;
+                a[1] = -2. * cosOmega;
+                a[2] = 1. - alpha;
+                break;
+            case AP:
+                alpha = sinOmega / (2. * Q);
+                b[0] = 1. - alpha;
+                b[1] = -2. * cosOmega;
+                b[2] = 1. + alpha;
+                a[0] = b[2];
+                a[1] = b[1];
+                a[2] = b[0];
+                break;
+            case NOTCH:
+                alpha = sinOmega * sinh(log(2.) / 2. * Q * omega / sinOmega);
+                b[0] = 1.;
+                b[1] = -2. * cosOmega;
+                b[2] = 1.;
+                a[0] = 1. + alpha;
+                a[1] = b[1];
+                a[2] = 1. - alpha;
+                break;
+            case PEAK:
+                alpha = sinOmega * sinh(log(2.) / 2. * Q * omega / sinOmega);
+                b[0] = 1. + (alpha * A);
+                b[1] = -2. * cosOmega;
+                b[2] = 1. - (alpha * A);
+                a[0] = 1. + (alpha / A);
+                a[1] = b[1];
+                a[2] = 1. - (alpha / A);
+                break;
+            case LS:
+                alpha = sinOmega / 2. * sqrt((A + 1. / A) * (1. / Q - 1.) + 2.);
+                b[0] = A * ((A + 1.) - ((A - 1.) * cosOmega) + (2. * sqrt(A) * alpha));
+                b[1] = 2. * A * ((A - 1.) - ((A + 1.) * cosOmega));
+                b[2] = A * ((A + 1.) - ((A - 1.) * cosOmega) - (2. * sqrt(A) * alpha));
+                a[0] = ((A + 1.) + ((A - 1.) * cosOmega) + (2. * sqrt(A) * alpha));
+                a[1] = -2. * ((A - 1.) + ((A + 1.) * cosOmega));
+                a[2] = ((A + 1.) + ((A - 1.) * cosOmega) - (2. * sqrt(A) * alpha));
+                break;
+            case HS:
+                alpha = sinOmega / 2. * sqrt((A + 1. / A) * (1. / Q - 1.) + 2.);
+                b[0] = A * ((A + 1.) + ((A - 1.) * cosOmega) + (2. * sqrt(A) * alpha));
+                b[1] = -2. * A * ((A - 1.) + ((A + 1.) * cosOmega));
+                b[2] = A * ((A + 1.) + ((A - 1.) * cosOmega) - (2. * sqrt(A) * alpha));
+                a[0] = ((A + 1.) - ((A - 1.) * cosOmega) + (2. * sqrt(A) * alpha));
+                a[1] = 2. * ((A - 1.) - ((A + 1.) * cosOmega));
+                a[2] = ((A + 1.) - ((A - 1.) * cosOmega) - (2. * sqrt(A) * alpha));
+                break;
+            }
+
+            // Normalize filter coefficients
+            const auto factor = 1. / a[0];
+
+            aCoef[0] = a[1] * factor;
+            aCoef[1] = a[2] * factor;
+
+            bCoef[0] = b[0] * factor;
+            bCoef[1] = b[1] * factor;
+            bCoef[2] = b[2] * factor;
+        }
+
+        void copyFrom(const BiquadFilter& other) noexcept
+        {
+            type = other.type;
+            for (auto i = 0; i < bCoef.size(); ++i)
+                bCoef[i] = other.bCoef[i];
+            for (auto i = 0; i < aCoef.size(); ++i)
+                aCoef[i] = other.aCoef[i];
+            omega = other.omega;
+            cosOmega = other.cosOmega;
+            sinOmega = other.sinOmega;
+            Q = other.Q;
+            alpha = other.alpha;
+            A = other.A;
+            for (auto i = 0; i < a.size(); ++i)
+                a[i] = other.a[i];
+            for (auto i = 0; i < b.size(); ++i)
+                b[i] = other.b[i];
+        }
+
+        Type type;
+
+        std::array<double, 3> bCoef; // b0, b1, b2
+        std::array<double, 2> aCoef; // a1, a2
+        std::array<double, 2> w; // delays
+
+        double omega, cosOmega, sinOmega, Q, alpha, A;
+        std::array<double, 3> a;
+        std::array<double, 3> b;
+
+        // DF-II impl
+        double processSample(double sample) noexcept
+        {
+            auto y = bCoef[0] * sample + w[0];
+            w[0] = bCoef[1] * sample - aCoef[0] * y + w[1];
+            w[1] = bCoef[2] * sample - aCoef[1] * y;
+            return y;
+        }
+    };
 
     using Buffer = juce::AudioBuffer<float>;
 
@@ -116,9 +300,9 @@ namespace wtc
             }
         }
 
-        void addFunction(const std::function<float(float)>& magFunc, 
-            const std::function<float(float)>& phaseFunc, 
-            const std::function<float(float)>& gainFunc, float x) noexcept
+        void addFunction(float x, const std::function<float(float)>& magFunc, 
+            const std::function<float(float)>& phaseFunc = [](float) { return 0.f; },
+            const std::function<float(float)>& gainFunc = [](float) { return 1.f; }) noexcept
         {
             const auto pitchStart = freqToPitch(1.f);
             const auto pitchEnd = freqToPitch(tableSizeF * .5f);
@@ -131,8 +315,35 @@ namespace wtc
             addMagAndPhase(gainFunc(x), phaseFunc(x), idx);
         }
 
+        void addFuncHarmonic(float x = 0.f) noexcept
+        {
+			addFunction
+            (
+                0.f,
+				[x](float) { return x; }
+			);
+		}
+
+        void normalizeFifo() noexcept
+        {
+            auto max = fifo[0].real();
+            for (auto i = 1; i < fftSize2; ++i)
+            {
+                const auto a = std::abs(fifo[i].real());
+                if (max < a)
+                    max = a;
+            }
+            if (max != 0.f)
+            {
+                auto gain = 1.f / max;
+                for (auto& f : fifo)
+                    f *= gain;
+            }
+        }
+
         void performInverseFFT() noexcept
         {
+            normalizeFifo();
             fft.perform(fifo.data(), bins.data(), true);
         }
 
@@ -154,6 +365,102 @@ namespace wtc
         juce::dsp::FFT fft;
         juce::Random randomizer;
     };
+
+    namespace fftFX
+    {
+        inline void distort(Specs& specs, float gain, float distance = 2.f, bool preventAliasing = true) noexcept
+        {
+            for (auto i = static_cast<int>(specs.fftSizeF / distance); i >= 0; --i)
+            {
+                const auto fc = static_cast<float>(i) / specs.fftSize2F;
+                const auto fc2 = fc * distance;
+                if (fc2 >= 0.f && fc2 < .5f)
+                {
+                    if (!preventAliasing || fc2 < specs.fftSizeF - 1.f)
+                    {
+                        const auto dFloor = fc2 * specs.fftSize2F;
+                        const auto d0 = static_cast<int>(dFloor);
+                        const auto d1 = d0 + 1;
+                        const auto dX = dFloor - static_cast<float>(d0);
+                        const auto dMag = specs.fifo[i].real() * gain;
+                        const auto dMag0 = dMag * (1.f - dX);
+                        const auto dMag1 = dMag * dX;
+                        const auto dPhs = specs.fifo[i].imag() * gain;
+                        const auto dPhs0 = dPhs * (1.f - dX);
+                        const auto dPhs1 = dPhs * dX;
+                        specs.addMagAndPhase(dMag0, dPhs0, d0);
+                        specs.addMagAndPhase(dMag1, dPhs1, d1);
+                    }
+                }
+            }
+        }
+
+        inline void blurBrk(Specs& specs, float fcWidth)
+        {
+            auto nFifo = specs.fifo;
+
+            for (auto i = 0; i < specs.fftSize; ++i)
+            {
+                const auto mag = specs.fifo[i].real() * .5f;
+                const auto phs = specs.fifo[i].imag() * .5f;
+                const auto iF = static_cast<float>(i);
+                const auto fc = iF / specs.fftSize2F;
+                const auto fc0 = fc - fcWidth;
+                if (fc0 > 0.f)
+                {
+                    const auto idx0 = static_cast<int>(std::round(fc0 * specs.fftSize2F));
+                    nFifo[idx0] += { mag, phs };
+                }
+                const auto fc1 = fc + fcWidth;
+                if (fc1 < 1.f)
+                {
+                    const auto idx1 = static_cast<int>(std::round(fc1 * specs.fftSize2F));
+                    nFifo[idx1] += { mag, phs };
+                }
+            }
+
+            specs.fifo = nFifo;
+        }
+
+        inline void blur(Specs& specs,
+            float fc, float reso, int slope) noexcept
+        {
+            BiquadFilter lpMag, lpPhase;
+            lpMag.setType(BiquadFilter::Type::LP);
+            lpMag.setCutoffFc(fc);
+            lpMag.setResonance(reso);
+            lpMag.update();
+            lpPhase.setType(BiquadFilter::Type::LP);
+            lpPhase.setCutoffFc(fc);
+            lpPhase.setResonance(reso);
+            lpPhase.update();
+
+            auto dest = specs.fifo;
+            auto src = specs.fifo.data();
+
+            const auto lp = [&](int i)
+            {
+                auto mag = static_cast<double>(src[i].real());
+                auto phs = static_cast<double>(src[i].imag());
+                mag = lpMag(mag);
+                phs = lpPhase(phs);
+                dest[i] = { static_cast<float>(mag), static_cast<float>(phs) };
+			};
+
+            for (auto i = specs.fftSize - 1; i != 0; --i)
+                lp(i);
+            for (auto i = 0; i < specs.fftSize; ++i)
+                lp(i);
+
+            for (auto i = 1; i < slope; ++i)
+            {
+                for (auto j = specs.fftSize - 1; j != 0; --j)
+                    lp(i);
+                for (auto j = 0; j < specs.fftSize; ++j)
+                    lp(i);
+            }
+        }
+    }
 
     using PrepareFunc = std::function<void(Specs&, int/*wavetable index*/)>;
     using TableFunc = std::function<float(Specs& specs, float/*x*/, int/*sample index*/, int/*wavetable index*/)>;
@@ -238,18 +545,56 @@ namespace wtc
             }
         }
 
-        // POST PROCESSING (MULTI)
-
-        void xFadeTables() noexcept
+        void applyHannWindowEachTable() noexcept
         {
-            for (auto i = 0; i < numTables - 1; ++i)
+            auto samples = buffer.getWritePointer(0);
+
+            for (auto i = 0; i < numTables; ++i)
             {
-                auto table0 = buffer.getWritePointer(0) + i * tableSize;
-                auto table1 = buffer.getWritePointer(0) + (i + 1) * tableSize;
+                auto k = i * tableSize;
 
                 for (auto j = 0; j < tableSize; ++j)
                 {
-                    auto x = static_cast<float>(j) / tableSize;
+					const auto hann = .5f * (1.f - std::cos(tau * static_cast<float>(j) * tableSizeInv));
+					samples[k + j] *= hann;
+				}
+            }
+        }
+
+        void xFadeTablesSingle(float lenRel)
+        {
+            std::vector<float> table2;
+            auto fadeLen = static_cast<int>(lenRel * static_cast<float>(tableSize));
+            table2.resize(fadeLen);
+
+            for (auto i = 0; i < numTables; ++i)
+            {
+                auto table = &buffer.getWritePointer(0)[i * tableSize];
+
+                for (auto j = 0; j < fadeLen; ++j)
+                    table2[j] = table[fadeLen - j - 1];
+
+                for (auto j = 0; j < fadeLen; ++j)
+                {
+                    auto idx = tableSize - fadeLen + j;
+                    auto x = (float)j * tableSizeInv;
+                    table[idx] += x * (table2[j] - table[idx]);
+                }
+            }
+        }
+
+        // POST PROCESSING (MULTI)
+
+        void xFadeTablesMulti() noexcept
+        {
+            for (auto i = 0; i < numTables - 1; ++i)
+            {
+                auto table0 = &buffer.getWritePointer(0)[i * tableSize];
+                auto table1 = &buffer.getWritePointer(0)[(i + 1) * tableSize];
+
+                for (auto j = 0; j < tableSize; ++j)
+                {
+                    auto x = static_cast<float>(j) * tableSizeInv;
                     table0[j] = table0[j] + x * (table1[j] - table0[j]);
                 }
             }
@@ -286,6 +631,14 @@ namespace wtc
             numTables = newNumTables;
             buffer = newBuffer;
         }
+
+        void upsampleNumTablesTo(int newNumTables)
+        {
+            if (newNumTables == numTables)
+                return;
+			const auto factor = static_cast<int>(static_cast<float>(newNumTables) / static_cast<float>(numTables));
+			upsampleNumTables(factor);
+		}
 
         // INTERPOLATION NumTables
         // ...
@@ -354,36 +707,29 @@ int main(int, char*)
 {
     using namespace wtc;
 
-    Specs specs(2048, 256);
+    Specs specs(2048, 64);
 
     PrepareFunc prepare = [](Specs& specs, int tableIdx)
     {
         specs.clearFifo();
         const auto x = static_cast<float>(tableIdx) / specs.numTablesF;
-        specs.addFunction
-        (
-            [](float) { return 0.f; },
-            [](float) { return 0.f; },
-            [](float) { return 1.f; },
-            x
-        );
-        auto len = 256.f;
-        for(auto i = 1.f; i <= len; ++i)
-            specs.addFunction
-            (
-                [i, len](float x)
+        
+        for (auto a = 0; a < 3; ++a)
+        {
+            specs.addFuncHarmonic(0.f);
+
+            specs.addFunction(x, [a](float x)
                 {
-                    const auto X = i / len;
-                    return -sin((1 + X) * cos((1 + 3 * X) * sin(1 + .0125 * X) * x * pi)) / 2.f + .5f;
-                },
-                [i, len](float x)
-                {
-                    const auto X = i / len;
-                    return -cos((1 + X) * sin((1 + 3 * X) * cos(1 + .0125 * X) * x * pi)) / 2.f + .5f;
-                },
-                [i, len](float) { return 1.f; },
-                x
-            );
+                    auto aa = (float)(1 << (a * 2));
+                    auto xx = x * aa;
+                    while (xx > 1.f)
+                        xx -= 1.f;
+                    return std::tan(xx * aa) / aa;
+                });
+            fftFX::blur(specs, .5f, 1.f, 3);
+            fftFX::distort(specs, .75f, 1 << (a + 1), false);
+        }
+        
         specs.performInverseFFT();
     };
 
@@ -394,9 +740,10 @@ int main(int, char*)
 
     Creator creator(specs, func, prepare);
 
-    //creator.upsampleNumTables(1);
     creator.dcOffsetEachTable();
+    //creator.xFadeTablesMulti();
     creator.normalizeEachTable();
+    creator.upsampleNumTablesTo(256);
     creator.makeWav("xxx");
 
     return 0;
